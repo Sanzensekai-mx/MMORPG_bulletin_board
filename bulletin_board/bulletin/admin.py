e8b40911e1e26c04fe52e85cf28e76e39320caba
SanZenSekai <45728342+Sanzensekai-mx@users.noreply.github.com>
2023-04-19 00:32:22 +0400

    fix: исправлено задвоение файлов в БД при сохранении

--- bulletin_board/bulletin/admin.py
+++ bulletin_board/bulletin/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import *
 from django import forms
+from django.db.models.fields.files import ImageFieldFile
 
 
 class FileInline(admin.TabularInline):
@@ -10,33 +11,37 @@ class FileInline(admin.TabularInline):
 
 class PostAdmin(admin.ModelAdmin):
     inlines = [FileInline]
+
     # exclude = ['main_image', 'load_files']
 
     def save_related(self, request, form, formsets, change):
-
-        print(self.get_inline_instances(request))
+        # print(formsets[0].cleaned_data)
+        super().save_related(request, form, formsets, change)
+        # print(formsets[0].cleaned_data)
+        # print(self.get_inline_instances(request))
         post = form.instance
-
-        post.load_files.clear()
-
         image_formset = formsets[0]
+
         if image_formset.cleaned_data:
             images_data = image_formset.cleaned_data
-            print(images_data)
+            # print(images_data)
             post.main_image = images_data[0]['upload_image']
             post.save()
 
+            images = []
             for image_data in images_data:
 
                 if image_data:
-                    print(image_data.get('upload_image'))
-                    print(Image.objects.filter(upload_image=image_data.get('upload_image')))
-                    # print(Image.objects.all().values('upload_image'))
-                    image = Image(post_rel=post, upload_image=image_data.get('upload_image'))
-                    image.save()
-                    post.load_files.add(image)
+                    # print(type(image_data.get('upload_image')))
+                    if type(image_data.get('upload_image')) is ImageFieldFile:
+                        image = Image.objects.filter(upload_image=image_data.get('upload_image').name).first()
+                    else:
+                        image = Image.objects.filter(
+                            upload_image=f"post_{post.id}/{image_data.get('upload_image').name}").first()
+                    images.append(image)
 
-            super().save_related(request, form, formsets, change)
+            post.load_files.clear()
+            post.load_files.set(images)
 
 
 # Register your models here.
