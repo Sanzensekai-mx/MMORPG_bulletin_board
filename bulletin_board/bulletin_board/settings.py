4387a0127bb7911105a147e50a00fec0f3780746
SanZenSekai <45728342+Sanzensekai-mx@users.noreply.github.com>
2023-04-18 18:36:59 +0400

    Инициализация All-auth

--- bulletin_board/bulletin_board/settings.py
+++ bulletin_board/bulletin_board/settings.py
@@ -40,7 +40,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bulletin',
+    'django.contrib.sites',  # Надо также для all-auth
+    # ! all-auth apps
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    # !
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -131,3 +139,24 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
+
+# all-auth main settings
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+LOGIN_URL = '/accounts/login/'
+
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_UNIQUE_EMAIL = True
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+LOGIN_REDIRECT_URL = '/all'
+ACCOUNT_LOGOUT_REDIRECT_URL = '/all'
