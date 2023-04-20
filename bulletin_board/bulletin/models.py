b1352800ffdc6ee501a86c3abe17714e3880126d
SanZenSekai <45728342+Sanzensekai-mx@users.noreply.github.com>
2023-04-20 17:15:45 +0400

    thumbnails для main images новые стили

--- bulletin_board/bulletin/models.py
+++ bulletin_board/bulletin/models.py
@@ -1,9 +1,11 @@
 import hashlib
+from io import BytesIO
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
-from PIL import Image
+from PIL import Image as PILImage
 
 
 # from mixins import ResizeImageMixin
@@ -27,6 +29,15 @@ def post_dir_path(instance, filename):
 def main_image_post_dir_path(instance, filename):
     # post = Post.objects.get(pk=instance.post_rel.id)
     return f'post_{instance.id}/main_{filename}'
+
+
+def make_thumbnail(main_image):
+    image = PILImage.open(main_image)
+    image.thumbnail((204, 204))
+    thumb_io = BytesIO()
+    image.save(thumb_io, image.format, quality=95)
+    thumbnail = InMemoryUploadedFile(thumb_io, None, f'{main_image.name}.jpeg', 'image/jpeg', thumb_io.tell(), None)
+    return thumbnail
 
 
 class Image(models.Model):
@@ -60,6 +71,23 @@ class Post(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     main_image = models.ImageField(upload_to=main_image_post_dir_path, blank=True)  # фиксированный размер должен быть
     load_files = models.ManyToManyField(to=Image, blank=True)  # все изображения, загружаемые пользователем
+
+    def save(self, *args, **kwargs):
+
+        if self.main_image:
+            print(1)
+            self.main_image = make_thumbnail(self.main_image)
+        # if not self.make_main_img_thumbnail():
+        #     pass
+        # print(1)
+        # print(self.main_image)
+        super().save(*args, **kwargs)
+        # file = self.main_image.name.split('.')
+        # image = PILImage.open(self.main_image.name, mode='r')
+        # image.thumbnail((204, 204), PILImage.ANTIALIAS)
+        # image.save(file + ".thumbnail", "JPEG")
+
+    #     image = Image.open(self.main_image)
 
     def __str__(self):
         return f'{self.title} | {self.create_datetime} | {self.author.username} | {self.category}'
