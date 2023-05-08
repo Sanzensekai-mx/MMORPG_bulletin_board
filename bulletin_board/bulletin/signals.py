33931dd3f671aeb024ebaf9d8507c76015a71608
SanZenSekai <45728342+Sanzensekai-mx@users.noreply.github.com>
2023-05-08 19:02:15 +0400

    Сигнал отправки уведомления об отклике

--- bulletin_board/bulletin/signals.py
+++ bulletin_board/bulletin/signals.py
@@ -1,9 +1,39 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from django.contrib.sites.models import Site
+from django.core.mail import EmailMultiAlternatives
 
-from .models import Reply
+from django.template.loader import render_to_string
+
+from .models import Reply, Post
 
 
 @receiver(post_save, sender=Reply)
-def notify_post_author_about_reply(sender, **kwargs):
-    print(kwargs)
+def notify_post_author_about_reply(sender, signal, instance, **kwargs):
+    domain = Site.objects.get_current().domain
+
+    reply = instance
+    post = instance.post
+
+    # !!!
+    url_to_post_detail = f'http://{domain}:8000{post.get_absolute_url()}'
+    # !!!
+
+    html_content = render_to_string(
+        template_name='email_reply_notify.html',
+        context={
+            'reply': reply,
+            'post': post,
+            'url_to_post_detail': url_to_post_detail,
+            'url_to_reply': None, # !!!!!
+        }
+    )
+    msg = EmailMultiAlternatives(
+        subject='На ваше объявление откликнулись!',
+        body=reply.text,
+        to=[post.author.email]
+    )
+    msg.attach_alternative(html_content, "text/html")
+    msg.send()
+
+
